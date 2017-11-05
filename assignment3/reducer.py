1e9de1885bb4dd29ea3ce924c71dcb8ea97db5fa
habibrahman0623 <bsse0623@iit.du.ac.bd>
2017-11-05 10:46:05 +0600

    a3 reducer updated

---
+++ assignment3/reducer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import operator
+import sys
+d = dict()
+for line in sys.stdin:
+    path = line.strip()
+    if path in d:
+      d[path] +=1
+    else: 
+      d[path] = 1
+
+print max(d.iteritems(), key=operator.itemgetter(1))[0]
